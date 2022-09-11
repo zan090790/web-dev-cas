8640ef6e87912f34f2d1ea3b68a6818165e946c4
FachriRAR <113273502+FachriRAR@users.noreply.github.com>
2022-09-11 10:38:07 +0700

    Add files via upload

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,7 +8,7 @@
     <title><?= htmlentities($title); ?></title>
 
     <!-- Local CSS -->
-    <link rel="stylesheet" href="../../../../css/style.css">
+    <link rel="stylesheet" href="../../../../css/stylenew.css">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
